074b7604d5b05f538f5467c0aec809db0bc89a07
akashSharma135 <akashsharma1352000@gmail.com>
2022-01-05 12:37:07 +0530

    fixed

--- api/__init__.py
+++ api/__init__.py
@@ -1,10 +1,10 @@
 from flask import Flask
 
-# def create_app():
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
 
-from api.pdf_converter import gen_pdf
+    from api.pdf_converter import gen_pdf
 
-app.register_blueprint(blueprint=gen_pdf, url_prefix='/')
+    app.register_blueprint(blueprint=gen_pdf, url_prefix='/')
 
-# return app
+    return app
